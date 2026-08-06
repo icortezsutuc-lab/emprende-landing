689aeba9fcb7264e33f0c30e693a025939cf5ff6
Ismael Enrique Cortez <icortezsutuc@gmail.com>
2026-08-05 20:59:11 -0600

    fix: corrige configuración final del proyecto

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import "../scss/main.scss";
 import "../js/main.js";
+import "../js/testimonios.js";
 
 import { RecursoController } from "./controllers/RecursoController";
 
 const controlador = new RecursoController();
 
 controlador.mostrarRecursos();
-
